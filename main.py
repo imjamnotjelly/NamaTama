3969d11c64e5b1a83f617a9d5ea01cf2eb26da3b
imjamnotjelly <81252683+imjamnotjelly@users.noreply.github.com>
2023-03-19 13:54:43 -0400

    comments (that's it)

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from os import getenv
+# Discord shit
 import discord
+import asyncio
+from os import getenv
+from dotenv import load_dotenv
+
+# Other shit
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
-import asyncio
-from dotenv import load_dotenv
 
+# Initializing Bot class
 bot = discord.Bot()
 
+# Emoji removal from string 
 def until_alpha(string: str):
     while not string[0].isalpha():
         string = string[1:]
     return string
 
+# The only fucking command!!!! Omg!!!
 @bot.slash_command(description="Generates a personlized LFG invite in the form of an embed or image")
 async def create_invite(
     ctx,
@@ -43,11 +49,14 @@ async def create_invite(
     "Embed",
     "Image"
     ]),
-
+    
+    # Option for second party member selection - refers to guild users
     second_player: discord.Option(discord.SlashCommandOptionType.user, required=False),
 
+    # Option for vc invitation (not really)
     voice_channel:  discord.Option(discord.SlashCommandOptionType.channel, required=False),
 ):
+    # Dynamic string relative to amount of existing party members
     players = [ctx.author.mention]
     if second_player:
         players.append(second_player.mention)
@@ -55,16 +64,20 @@ async def create_invite(
     players = f"{', '.join(players)}: {len(players)}/3"
 
     if output == "Embed":
+        # Embed generation 
+        # Hashmap for embed config
         details = {
             "Rank":rank,
             "Gamemode":gamemode,
             "Time":time,
             "Players":players,
         }
-
+        
+        # Adds voice_channel param to embed if not null 
         if voice_channel:
             details["Voice Channel"] = f"[Join channel]({str(await voice_channel.create_invite())})"
-
+            
+        # Embed creation
         embed = discord.Embed(
             title="The Finals - NA LFG",
             description=f"{ctx.author.name} is looking for a group!",
@@ -77,10 +90,17 @@ async def create_invite(
             embed.add_field(name=k, value=v, inline=True)
         
         await ctx.respond(embed=embed)
-    """ else:
+    else:
+        # Image banner generation (because god knows there's no way embark  would actually let this shitty passion project actually add embeds to the server)
         ctx.respond("holup")
         img = Image.open("assets/banner.png")
-        img = ImageDraw.Draw(img) """
+        img = ImageDraw.Draw(img)
+        fonts = {
+              "Medium":"assets/fonts/SairaRegular.ttf",
+              "Bold":"assets/fonts/SairaBold.ttf"
+        }
+ 
+        
 
 load_dotenv()
 TOKEN = str(getenv("TOKEN"))
